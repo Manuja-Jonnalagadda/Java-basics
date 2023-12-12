9522540488319a078191e80e2a5d33ddc3526772
Manuja-Jonnalagadda <153074196+Manuja-Jonnalagadda@users.noreply.github.com>
2023-12-12 11:51:31 +0530

    Primitive data types default values in java

---
+++ Variables.java
@@ -0,0 +1,24 @@
+public class Variables
+{
+	static int a;
+	static byte b;
+	static short c;
+	static long d;
+	static boolean e;
+	static char f;
+	static float g;
+	static double h;
+	public static void func() {
+    byte cy_inc = 127;
+    System.out.println(cy_inc+1);
+		//default values of above class variables
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+	}
+}
