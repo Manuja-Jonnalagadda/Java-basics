17eb7e2bee4097ddbe9d60359e2286a9a1691d50
Manuja-Jonnalagadda <153074196+Manuja-Jonnalagadda@users.noreply.github.com>
2023-12-12 11:52:53 +0530

    Update Variables.java

--- Variables.java
+++ Variables.java
@@ -9,8 +9,8 @@ public class Variables
 	static float g;
 	static double h;
 	public static void func() {
-    byte cy_inc = 127;
-    System.out.println(cy_inc+1);
+		byte cy_inc = 127;
+		System.out.println(cy_inc+1);
 		//default values of above class variables
 		System.out.println(a);
 		System.out.println(b);
